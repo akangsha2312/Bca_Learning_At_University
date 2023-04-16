20ceed122642f2c96167bbedc9054276327d6cd0
akangsha2312 <126556906+akangsha2312@users.noreply.github.com>
2023-04-16 22:32:37 +0530

    Update Assignment13.c

--- Assignment13.c
+++ Assignment13.c
@@ -5,8 +5,44 @@
 //Press 4 for division
 
 //after showing result the main menu should appear again.
-
-int main()
+#include<stdio.h>
+void main()
 {
-  return 0;
+  int num1,num2,opt;
+  printf("Enter the first Integer :");
+  scanf("%d",&num1);
+  printf("Enter the second Integer :");
+  scanf("%d",&num2);
+  
+    printf("\nInput your option :\n");
+    printf("1-Addition.\n2-Substraction.\n3-Multiplication.\n4-Division.\n");
+    scanf("%d",&opt);
+    switch(opt) {
+      case 1:
+        printf("The Addition of  %d and %d is: %d\n",num1,num2,num1+num2);
+        break;
+        
+      case 2:
+        printf("The Substraction of %d  and %d is: %d\n",num1,num2,num1-num2);
+        break;
+        
+      case 3:
+        printf("The Multiplication of %d  and %d is: %d\n",num1,num2,num1*num2);
+        break;  
+      
+      case 4:
+        if(num2==0) {
+          printf("The second integer is zero. Devide by zero.\n");
+        } else {
+          printf("The Division of %d  and %d is : %d\n",num1,num2,num1/num2);
+        }  
+        break;
+        
+      case 5: 
+        break; 
+        
+      default:
+        printf("Input correct option\n");
+        break; 
+    }
 }

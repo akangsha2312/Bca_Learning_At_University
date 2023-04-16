21e1fa1705591776bbabe56836e42149cec75319
akangsha2312 <126556906+akangsha2312@users.noreply.github.com>
2023-04-16 22:45:32 +0530

    Update Assignment4.c

--- Assignment4.c
+++ Assignment4.c
@@ -1,7 +1,24 @@
 //Write a C code to find the maximum element in each row of the 3 X 3 matrix
 
+#include<stdio.h>
 int main()
 {
-  
-  return 0;
+    int mat[3][3], i, j, max;
+    printf("Enter any 3*3 matrix: ");
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+            scanf("%d", &mat[i][j]);
+    }
+    max = mat[0][0];
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            if(max<mat[i][j])
+                max = mat[i][j];
+        }
+    }
+    printf("\nLargest Element = %d", max);
+    return 0;
 }

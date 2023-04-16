99805212039e0c02d0be7d590fc747527c3828ec
akangsha2312 <126556906+akangsha2312@users.noreply.github.com>
2023-04-16 23:05:23 +0530

    Update Assignment9.c

--- Assignment9.c
+++ Assignment9.c
@@ -5,8 +5,32 @@
 //.       m = 1
 //.       n = 1
 
-int main()
-{
-  
-  return 0;
-}
+#include <stdio.h>  
+#include <string.h>  
+   
+int main()  
+{  
+    char string[] = "raman";  
+    int i, j, length = strlen(string);  
+    int freq[length];  
+      
+      
+    for(i = 0; i < strlen(string); i++) {  
+        freq[i] = 1;  
+        for(j = i+1; j < strlen(string); j++) {  
+            if(string[i] == string[j]) {  
+                freq[i]++;  
+                string[j] = '0';  
+            }  
+        }  
+    }  
+      
+    //Displays the each character and their corresponding frequency  
+    printf("Characters and their corresponding frequencies\n");  
+    for(i = 0; i < length; i++) {  
+        if(string[i] != ' ' && string[i] != '0')  
+            printf("%c-%d\n", string[i], freq[i]);  
+    }  
+          
+    return 0;  
+}  
